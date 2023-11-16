904e934756e4a2a04ec503e08a5c34723cc7f7b7
jamesw732 <jamesmw732@gmail.com>
2023-11-16 18:38:07 -0500

    logistic: uses cross-validation instead of test set

--- code/Logistic/logistic.py
+++ code/Logistic/logistic.py
@@ -1,40 +1,30 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate
 import pandas as pd
 import numpy as np
 import pathlib
 import os
 
 # Perform logistic regression with two definitions for "good question":
-# 1. score >= 0
+# 1. score > 0 (bad means score < 0, so drop score == 0)
 # 2. is_answered
-# Don't use metadata features for this, as we are interested in interpretability.
+# Goal of this is to decide which definition to use for SVM/Trees. Metadata features seem bad to include here. Source?
 
 dir = os.path.join(pathlib.Path(__file__).parent.resolve())
+datadir = os.path.abspath(os.path.join(pathlib.Path(__file__).parent.resolve(), '../../data'))
 
 # Score:
-train = pd.read_csv(os.path.join(dir, 'score_train.csv'))
-good_score = train['good_score']
-X_train = train[train.columns.drop(['good_score'])]
-test = pd.read_csv(os.path.join(dir, 'score_test.csv'))
-good_score_test = test['good_score']
-X_test = test[test.columns.drop(['good_score'])]
+train = pd.read_csv(os.path.join(datadir, 'train.csv'))
+good_score = train['score'] > 0
+is_answered = train['is_answered']
+# Controversial lines:
+metadata = ['score', 'owner_rep', 'owner_accept_rate', 'is_closed', 'view_count', 'answer_count', 'is_answered']
+train = train[train.columns.drop(metadata)]
+# drop = ['score', 'is_answered']
+# train = train[train.columns.drop(drop)]
 
-lr = LogisticRegression().fit(X_train, good_score)
-
-predict = lr.predict(X_test)
-error = len(np.where(predict != good_score_test)[0]) / len(good_score_test)
-print(f"Classification error using score as definition of good question: {error * 100}%")
-
-
-train = pd.read_csv(os.path.join(dir, 'answered_train.csv'))
-answered = train['is_answered']
-X_train = train[train.columns.drop(['is_answered'])]
-test = pd.read_csv(os.path.join(dir, 'answered_train.csv'))
-answered_test = test['is_answered']
-X_test = test[test.columns.drop(['is_answered'])]
-
-lr = LogisticRegression(max_iter=1000).fit(X_train, answered)
-
-predict = lr.predict(X_test)
-error = len(np.where(predict != answered_test)[0]) / len(answered_test)
-print(f"Classification error using answered/not answered as definition of good question: {error * 100}%")
+lr = LogisticRegression(max_iter=1000)
+cv = cross_validate(lr, train, good_score, scoring='accuracy')
+print(f"Mean cross-validation score using positive score as definition of good question: {np.mean(cv['test_score'])}")
+cv = cross_validate(lr, train, is_answered, scoring='accuracy')
+print(f"Mean cross-validation score using answered/not answered as definition of good question: {np.mean(cv['test_score'])}")
