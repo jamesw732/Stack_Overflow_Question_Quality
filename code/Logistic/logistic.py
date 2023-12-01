7396141699cfa12092fd5148b82145e65ed18a57
jamesw732 <jamesmw732@gmail.com>
2023-12-01 16:51:46 -0500

    Removed the is_answered stuff from logistic

--- code/Logistic/logistic.py
+++ code/Logistic/logistic.py
@@ -4,25 +4,17 @@ import pandas as pd
 import numpy as np
 import os
 
-# Perform logistic regression with two definitions for "good question":
-# 1. score > 0 (bad means score < 0, so drop score == 0)
-# 2. is_answered
-# Goal of this is to decide which definition to use for SVM/Trees. Metadata features seem bad to include here. Source?
+# Train Logistic regression on the training set and measure error on the test set.
 
 datadir = os.path.abspath(os.path.join(os.path.realpath(__file__), '../../../data'))
 
 # Score:
 train = pd.read_csv(os.path.join(datadir, 'train.csv'))
 good_score = train['score'] > 0
-is_answered = train['is_answered']
 drop = ['score', 'is_answered']
 train = train[train.columns.drop(drop)]
 
 lr = LogisticRegression(max_iter=1000)
-cv = cross_validate(lr, train, good_score, scoring='accuracy')
-print(f"Mean cross-validation score using positive score as definition of good question: {np.mean(cv['test_score'])}")
-cv = cross_validate(lr, train, is_answered, scoring='accuracy')
-print(f"Mean cross-validation score using answered/not answered as definition of good question: {np.mean(cv['test_score'])}")
 
 test = pd.read_csv(os.path.join(datadir, 'test.csv'))
 test_good_score = test['score'] > 0
